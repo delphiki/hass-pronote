1b61628f0ddbbdf18e349b4afafe2bd576b2c056
Philippe Gressé-Lugué <GaPhi@users.noreply.github.com>
2024-11-05 22:03:42 +0100

    Support 2FA with pronotepy 2.14.0 (#82)
    * Support malformed expected url
    
    * Support 2FA with pronotepy 2.14.0
    
    * Implement qrCode login with 2FA

--- custom_components/pronote/pronote_helper.py
+++ custom_components/pronote/pronote_helper.py
@@ -3,6 +3,7 @@
 import pronotepy
 import json
 import logging
+import re
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -15,7 +16,11 @@ def get_pronote_client(data) -> pronotepy.Client | pronotepy.ParentClient | None
         return get_client_from_username_password(data)
 
 def get_client_from_username_password(data) -> pronotepy.Client | pronotepy.ParentClient | None:
-    url = data['url'] + ('parent' if data['account_type'] == 'parent' else 'eleve') + '.html'
+    url = data['url']
+    url = re.sub(r'/[^/]+\.html$', '/', url)
+    if not url.endswith('/'):
+        url += '/'
+    url = url + ('parent' if data['account_type'] == 'parent' else 'eleve') + '.html'
 
     ent = None
     if 'ent' in data:
@@ -26,12 +31,16 @@ def get_client_from_username_password(data) -> pronotepy.Client | pronotepy.Pare
 
     try:
         client = (pronotepy.ParentClient if data['account_type'] == 'parent' else pronotepy.Client)(
-            url,
-            data['username'],
-            data['password'],
-            ent
+            pronote_url =url,
+            username=data['username'],
+            password=data['password'],
+            account_pin=data.get('account_pin', None),
+            device_name=data.get('device_name', None),
+            client_identifier=data.get('client_identifier', None),
+            ent=ent
         )
         del ent
+        del client.account_pin
         _LOGGER.info(client.info.name)
     except Exception as err:
         _LOGGER.critical(err)
@@ -50,29 +59,41 @@ def get_client_from_qr_code(data) -> pronotepy.Client | pronotepy.ParentClient |
 
         # get the initial client using qr_code
         client = (pronotepy.ParentClient if data['account_type'] == 'parent' else pronotepy.Client).qrcode_login(
-            qr_code_json,
-            qr_code_pin,
-            uuid
+            qr_code=qr_code_json,
+            pin=qr_code_pin,
+            uuid=uuid,
+            account_pin=data.get('account_pin', None),
+            client_identifier=data.get('client_identifier', None),
+            device_name=data.get('device_name', None)
         )
 
         qr_code_url = client.pronote_url
         qr_code_username = client.username
         qr_code_password = client.password
         qr_code_uuid = client.uuid
+        qr_code_account_pin = client.account_pin
+        qr_code_device_name = client.device_name
+        qr_code_client_identifier = client.client_identifier
     else:
         qr_code_url = data['qr_code_url']
         qr_code_username = data['qr_code_username']
         qr_code_password = data['qr_code_password']
         qr_code_uuid = data['qr_code_uuid']
+        qr_code_account_pin = data.get('account_pin', None)
+        qr_code_device_name = data.get('device_name', None)
+        qr_code_client_identifier = data.get('client_identifier', None)
 
     _LOGGER.info(f"Coordinator uses qr_code_username: {qr_code_username}")
     _LOGGER.info(f"Coordinator uses qr_code_pwd: {qr_code_password}")
 
     return (pronotepy.ParentClient if data['account_type'] == 'parent' else pronotepy.Client).token_login(
-        qr_code_url,
-        qr_code_username,
-        qr_code_password,
-        qr_code_uuid
+        pronote_url=qr_code_url,
+        username=qr_code_username,
+        password=qr_code_password,
+        uuid=qr_code_uuid,
+        account_pin=qr_code_account_pin,
+        device_name=qr_code_device_name,
+        client_identifier=qr_code_client_identifier
     )
 
 def get_day_start_at(lessons):
