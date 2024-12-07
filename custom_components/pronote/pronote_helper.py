77346cfd4e681fa7c89ad4a8e54c09b9514082bc
Julien Villetorte <julien.villetorte@evaneos.com>
2024-12-07 10:46:41 +0100

    fix(autoslots): use hotfix from @bain3 for python 3.13

--- custom_components/pronote/pronote_helper.py
+++ custom_components/pronote/pronote_helper.py
@@ -1,5 +1,26 @@
 """Client wrapper for the Pronote integration."""
 
+### Hotfix for python 3.13 https://github.com/bain3/pronotepy/pull/317#issuecomment-2523257656
+import autoslot
+from itertools import tee
+import dis
+def assignments_to_self(method) -> set:
+    instance_var = next(iter(method.__code__.co_varnames), 'self')
+    instructions = dis.Bytecode(method)
+    i0, i1 = tee(instructions)
+    next(i1, None)
+    names = set()
+    for a, b in zip(i0, i1):
+        accessing_self = (
+            a.opname in ("LOAD_FAST", "LOAD_DEREF") and a.argval == instance_var
+        ) or (a.opname == "LOAD_FAST_LOAD_FAST" and a.argval[1] == instance_var)
+        storing_attribute = b.opname == "STORE_ATTR"
+        if accessing_self and storing_attribute:
+            names.add(b.argval)
+    return names
+autoslot.assignments_to_self = assignments_to_self
+### End Hotfix
+
 import pronotepy
 import json
 import logging
